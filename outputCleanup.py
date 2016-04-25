6e7287095cd9e6afbd278579077a45236ccd7968
Benjamin Hering <benjamin.hering@gmail.com>
2016-04-25 05:28:26 +0000

    Add shiny cycle through colors on boot

--- outputCleanup.py
+++ outputCleanup.py
@@ -1,6 +1,21 @@
 import RPi.GPIO as GPIO
+import time
 
+# Set pin mode, set pins to output and clear outputs to false
 GPIO.setmode(GPIO.BCM)
 GPIO.setup([5,6,13], GPIO.OUT)
 GPIO.output([5,6,13], False)
+
+# Turns a pin on, waits and then turns off
+def pinToggle(pin, duration):
+	GPIO.output(pin, True)
+	time.sleep(duration)
+	GPIO.output(pin, False)
+
+# Cycle through the colors
+pinToggle(5, 2)
+pinToggle(6, 2)
+pinToggle(13, 2)
+
+# Clear everything back to False, cleanup and exit
 GPIO.cleanup()
